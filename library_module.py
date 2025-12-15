f11d5b7f8fff2ca885643b31cdedad8038d09983
Ashlan101 <dght1027@gmail.com>
2025-12-15 11:30:56 +0800

    Delete library_module.py

--- library_module.py
+++
@@ -1,8 +0,0 @@
-class Book:
-    def __init__(self,title, author):
-        self.title = title
-        self.author = author
-
-    def display_info(self):
-        print(f"Title: {self.title}, Author:{self.author}")
-        
